192abaee4e5ab29a3fb19e40c5a85ee16ead5198
cumul <gg6123@naver.com>
2019-03-19 15:56:21 +0900

    [Web] 고정폭 레이아웃으로 변경

--- web/src/components/AppBar.tsx
+++ web/src/components/AppBar.tsx
@@ -4,9 +4,7 @@ import classes from './AppBar.mcss';
 
 const AppBar: React.SFC<Props> = ({ className, children, ...props }): React.ReactElement => (
   <div className={cx(classes.container, className)} {...props}>
-    <div className={classes.wrapper}>
-      <span className={classes.title}>{children}</span>
-    </div>
+    <h1 className={cx(classes.wrapper, classes.title)}>{children}</h1>
   </div>
 );
 
