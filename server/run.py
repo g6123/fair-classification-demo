8fb50ebe9be420b513756ec8a2c48381268ce3ac
cumul <gg6123@naver.com>
2019-03-22 04:11:39 +0900

    [Server] 데이터셋 조회 및 분류 기능 추가

--- server/run.py
+++ server/run.py
@@ -1,6 +1,11 @@
+import json
+from pprint import pprint
+
 import asyncio
 import websockets
-import json
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 from app.datasets import datasets
 
@@ -9,18 +14,41 @@ async def handle(ws, path):
     async for message in ws:
         message = json.loads(message)
 
-        if message['type'] == 'dataset':
-            response = {'type': 'dataset', 'name': message['name']}
+        if message['type'] == 'FETCH_DATASET':
+            response = {'type': 'SET_DATASET', 'name': message['name']}
 
             Dataset = datasets[message['name']]
-            dataset = Dataset(message.get('options', {}))
+            dataset = Dataset()
 
             response.update(dataset.df.to_dict(orient='split'))
             response.pop('index')
 
             await ws.send(json.dumps(response))
+        elif message['type'] == 'APPLY_METHOD':
+            Dataset = datasets[message['dataset']]
+            dataset = Dataset()
+
+            if message['method']['type'] == 'none':
+                if message['method']['classifier'] == 'lr':
+                    lr = LinearRegression().fit(dataset.x, dataset.y)
+                    y_ = lr.predict(dataset.x)
+                    y_ = np.stack((y_, y_ * -1), axis=-1)
+
+                    await ws.send(json.dumps({
+                        'type': 'SET_POINTS',
+                        'positions': y_.tolist(),
+                        'predictions': np.argmax(y_, axis=1).tolist(),
+                        'grounds': dataset.y.tolist()
+                    }))
+                # elif message['method']['classifier'] == 'nn':
+                #     pass
+                else:
+                    await ws.send(json.dumps({'type': 'SET_ERROR', 'message': 'Unkown classifier type'}))
+            else:
+                await ws.send(json.dumps({'type': 'SET_ERROR', 'message': 'Unkown bias mitigation method'}))
         else:
-            await ws.send({'type': 'error', 'message': 'Unknown action type'})
+            await ws.send(json.dumps({'type': 'SET_ERROR', 'message': 'Unknown action type'}))
+
 
 serve = websockets.serve(handle, 'localhost', 9000)
 
